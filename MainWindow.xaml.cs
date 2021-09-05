ad3f184d98aab4af08e08e9035f281b5c7ff9489
ZyrianCode <79591461+ZyrianCode@users.noreply.github.com>
2021-09-05 13:07:09 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFCalculator.Zyrian.Validator;
+
+namespace WPFCalculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            foreach (UIElement element in MainGrid.Children)
+            {
+                if (element is Button)
+                {
+                    ((Button)element).Click += ButtonClick;
+                }
+            }
+        }
+
+        SimpleExpressionValidator validator = new SimpleExpressionValidator();
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string buttonContent = (string)((Button)e.OriginalSource).Content;
+
+            switch (buttonContent)
+            {
+                
+                case "EC": Clear(); break;
+                case "C": Delete(); break;
+                case "=":
+                    {
+                        if (validator.IsSecondOperandExist(resultLabel.Text))
+                        {
+                            resultLabel.Text += validator.AddSecondOperand();
+                        }
+                        string value = new DataTable().Compute(resultLabel.Text, null).ToString();
+                        resultLabel.Text = value;
+                        break;
+                    }
+                default:
+                    resultLabel.Text += buttonContent;
+                    break;
+            }
+        }
+
+        private void Clear() => resultLabel.Text = "";
+
+        private void Delete()
+        {
+            string initialString = resultLabel.Text;
+            if (initialString.Length > 0 && initialString != null)
+            {
+                resultLabel.Text = initialString.Remove(initialString.IndexOf(initialString.Last()), 1);
+            }             
+        }
+    }
+}
---
+++ Zyrian/Validator/SimpleExpressionValidator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCalculator.Zyrian.Validator
+{
+    class SimpleExpressionValidator
+    {
+        private List<string> AllOperators = new List<string>() {
+            "/", "*", "-", "+"
+        };
+
+        private string addableOperand = "0";
+
+        public bool IsExpressionEmpty(string expression) => !expression.Any();
+
+        public bool IsSecondOperandExist(string expression)
+        {
+            if (!IsExpressionEmpty(expression) && IsContainsOperator(expression))
+            {
+                string rightSide = "";
+                int index = 0;
+                int checkLength = 0;
+
+                for (index = 0; !IsOperator(expression[index]); ++index) { }
+                for (int i = index; !NextSymbolIsVoid(i, expression) && !IsNumericOperand(expression[i]); ++i)
+                {
+                    ++checkLength;
+                }
+                
+                for (int i = index + checkLength; i < expression.Length; i++)
+                {
+                    rightSide += expression[i];
+                }
+                if (rightSide.Any() && IsNumericOperand(rightSide.First()))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsOperator(char symbol) => (AllOperators.Contains(symbol.ToString())) ? true : false;
+
+        public bool IsNumericOperand(char symbol) => !IsOperator(symbol);
+
+        public bool IsContainsOperator(string expression)
+        {
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (IsOperator(expression[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string AddSecondOperand() => addableOperand;
+
+        public string FillExpression() => addableOperand;
+
+        private bool NextSymbolIsVoid(int i, string expression) => i >= expression.Length ? true : false;
+    }
+}
